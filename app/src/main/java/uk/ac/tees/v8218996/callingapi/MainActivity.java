b12caeaf345c14f731767fa4cd5d7a37787d6d9d
edenated@gmail.com <gazelleinc.1z>
2020-02-25 14:15:21 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/v8218996/callingapi/MainActivity.java
@@ -0,0 +1,91 @@
+package uk.ac.tees.v8218996.callingapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+   //private RequestQueue requestQueue;
+   RequestQueue queue;
+   
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         //requestQueue = Volley.newRequestQueue(this);
+        queue = MySingleton.getInstance(this.getApplicationContext())
+                            .getRequestQueue();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos/1", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            Log.d("response", "onResponse: " + response.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+//                        Log.d("response", "onResponse: " + response);
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    Log.d("error", "onErrorResponse: " + error.getMessage());
+                }
+        });
+
+        //JsonArrayRequest
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("response", "onResponse: " + response);
+
+                        for (int i=0; i < response.length(); i++){
+
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Log.d("geteachobj", "onResponse: " + jsonObject.getString("title"));
+                                Log.d("geteachobj", "onResponse: " + jsonObject.getBoolean("completed"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("error", "onErrorResponse: " + error.getMessage());
+                }
+        });
+        //JsonObjectRequest
+        //requestQueue.add(jsonObjectRequest);
+
+        //requestQueue.add(jsonArrayRequest);
+
+        queue.add(jsonArrayRequest);
+    }
+
+
+}
